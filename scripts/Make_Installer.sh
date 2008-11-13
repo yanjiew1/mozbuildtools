f489e37f18c4320c76dc3b27b0f9893fc1d31850
Jay Wang <stu90206@gmail.com>
2008-11-13 13:53:39 +0000

    將 tools 內執行檔用 UPX 壓縮 將 NSPR 的 DLL 用 UPX 壓縮 移除 make_installer.sh

--- scripts/Make_Installer.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-cp `cat /root`/NSPR/bin/*.dll dist/bin/ > /dev/null 2> /dev/null
-cp `cat /root`/moztools/bin/*.dll dist/bin/ > /dev/null 2> /dev/null
-make installer > /dev/null 2> /dev/null
-mkdir `cat /root`/installer > /dev/null 2> /dev/null
-cp dist/install/sea/*.exe `cat /root`/installer > /dev/null 2> /dev/null
